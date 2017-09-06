8deb59a0bb4f45ead45733d60aac9c58f0145ac8
Anubhav Trivedi <anu.bhavtrivedi111@gmail.com>
2017-09-06 11:04:36 +0530

    Update BreathFirstSearch.java

--- main/graphAlgorithms/BreathFirstSearch.java
+++ main/graphAlgorithms/BreathFirstSearch.java
@@ -2,13 +2,13 @@
 
 public class BreathFirstSearch {
 	public static void bfs(Vertex[] vert, int start, int n) {
-		Vertex[] stack = new Vertex[n+1];
+		Vertex[] queue = new Vertex[n+1];
 		int top = 0, bottm = 0, v1, v2;
 		Vertex vt;
 		int[] status = new int[n+1];
-		stack[top] = vert[start];
+		queue[top] = vert[start];
 		while (top >= bottm) {
-			vt = stack[bottm];
+			vt = queue[bottm];
 			status[vt.name] = 1;
 			bottm++;
 
@@ -20,12 +20,12 @@ public class BreathFirstSearch {
 				if (status[v1] == 0) {
 					status[v1] = 1;
 					top++;
-					stack[top] = vert[v1];
+					queue[top] = vert[v1];
 				}
 				if (status[v2] == 0) {
 					status[v2] = 1;
 					top++;
-					stack[top] = vert[v2];
+					queue[top] = vert[v2];
 				}
 
 			}
